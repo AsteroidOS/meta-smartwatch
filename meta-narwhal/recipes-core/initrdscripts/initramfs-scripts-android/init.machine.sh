a2331ac1c7f77dfe55a042bc2114ca679c09282c
MagneFire <IDaNLContact@gmail.com>
2021-09-26 12:45:20 +0200

    narwhal: initrdscripts: Automatically mount partitions.

--- meta-narwhal/recipes-core/initrdscripts/initramfs-scripts-android/init.machine.sh
+++ meta-narwhal/recipes-core/initrdscripts/initramfs-scripts-android/init.machine.sh
@@ -9,9 +9,6 @@ BOOT_DIR=$1
 mkdir $BOOT_DIR/nvdata
 mount -t ext4 /dev/mmcblk0p9 $BOOT_DIR/nvdata
 
-mkdir $BOOT_DIR/firmware
-mount -t ext4 /dev/mmcblk0p32 $BOOT_DIR/firmware
-
 mkdir -p /nvdata
 mkdir -p /firmware
 mkdir -p /system/etc/wifi
