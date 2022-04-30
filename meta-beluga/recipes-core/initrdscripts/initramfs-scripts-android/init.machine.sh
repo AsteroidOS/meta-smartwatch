9162d0cf04ba794b7239d1159f4184c3c8dc727e
Darrel Griët <dgriet@gmail.com>
2022-04-30 22:20:21 +0200

    beluga: initrdscripts: Show prompt when an invalid rootfs was detected. Beluga uses an encrypted userdata partition which makes it more dificult to push the .ext4 once booted into WearOS. Once must push the .ext4 after performing a factory reset but before booting into WearOS.
    Signed-off-by: Darrel Griët <dgriet@gmail.com>

---
+++ meta-beluga/recipes-core/initrdscripts/initramfs-scripts-android/init.machine.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+BOOT_DIR=$1
+
+# Check if the AsteroidOS specific machine configuration file exists.
+# If it doesn't then we know that the userdata partition is mounted but doesn't contain a valid AsteroidOS root.
+if [ ! -e $BOOT_DIR/etc/asteroid/machine.conf ] ; then
+    echo MSG "Push asteroidos.ext4 via ADB" > /run/psplash_fifo
+
+    # Setup for use with ConfigFS
+    /usr/bin/android-gadget-setup adb
+
+    # Correct properties as /system/ may not exist.
+    serial="$(cat /proc/cmdline | sed 's/.*androidboot.serialno=//' | sed 's/ .*//')"
+    echo $serial > /sys/kernel/config/usb_gadget/g1/strings/0x409/serialnumber
+    echo OPPO > /sys/kernel/config/usb_gadget/g1/strings/0x409/manufacturer
+    echo OPPO Watch > /sys/kernel/config/usb_gadget/g1/strings/0x409/product
+
+    /usr/bin/adbd
+fi
