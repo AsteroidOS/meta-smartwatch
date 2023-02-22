d11190a0b587fc31d01ea3adb2a4fe8cd1c63f7e
Arseniy Movshev <dodoradio@outlook.com>
2023-02-22 23:09:38 +0000

    medaka: add back the complex android 9 system mount process from beluga the system dir actually contains the boot ramdisk, which means that the partition must be remounted from there. this is accomplished by the machine-specific init script.

--- meta-medaka/recipes-core/initrdscripts/initramfs-scripts-android/init.machine.sh
+++ meta-medaka/recipes-core/initrdscripts/initramfs-scripts-android/init.machine.sh
@@ -4,7 +4,7 @@ BOOT_DIR=$1
 
 # Manually mount the system partition as it contains the boot image (ramdisk) as well as the system data (under /system/system).
 mkdir -m 0777 $BOOT_DIR/boot
-mount -t auto -o ro /dev/mmcblk0p36 $BOOT_DIR/boot
+mount -t auto -o ro /dev/mmcblk0p2 $BOOT_DIR/boot
 
 # Allow Android binaries to load libraries from /usr/libexec/ (for example /usr/libexec/hal-droid/system/lib/libselinux_stubs.so)
 mount --bind /ld.config.28.txt $BOOT_DIR/boot/system/etc/ld.config.28.txt
@@ -23,8 +23,8 @@ if [ ! -e $BOOT_DIR/etc/asteroid/machine.conf ] ; then
     # Correct properties as /system/ may not exist.
     serial="$(cat /proc/cmdline | sed 's/.*androidboot.serialno=//' | sed 's/ .*//')"
     echo $serial > /sys/kernel/config/usb_gadget/g1/strings/0x409/serialnumber
-    echo OPPO > /sys/kernel/config/usb_gadget/g1/strings/0x409/manufacturer
-    echo OPPO Watch > /sys/kernel/config/usb_gadget/g1/strings/0x409/product
+    echo CASIO > /sys/kernel/config/usb_gadget/g1/strings/0x409/manufacturer
+    echo WSD-F21hr > /sys/kernel/config/usb_gadget/g1/strings/0x409/product
 
     /usr/bin/adbd
 fi
--- meta-medaka/recipes-core/initrdscripts/initramfs-scripts-android_1.0.bbappend
+++ meta-medaka/recipes-core/initrdscripts/initramfs-scripts-android_1.0.bbappend
@@ -1,5 +1,14 @@
 FILESEXTRAPATHS:prepend:medaka := "${THISDIR}/${PN}:"
 COMPATIBLE_MACHINE:medaka = "medaka"
 
-RDEPENDS:${PN}:append:medaka = " msm-fb-refresher"
+SRC_URI:append:medaka = " file://init.machine.sh \
+    file://ld.config.28.txt"
 
+do_install:append:medaka() {
+    install -m 0755 ${WORKDIR}/init.machine.sh ${D}/init.machine
+    install -m 0755 ${WORKDIR}/ld.config.28.txt ${D}/ld.config.28.txt
+}
+
+
+RDEPENDS:${PN}:append:medaka = " msm-fb-refresher"
+FILES:${PN}:append:medaka = " /ld.config.28.txt"
